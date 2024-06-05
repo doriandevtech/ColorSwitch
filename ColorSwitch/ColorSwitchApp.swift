2156d0374058b64b7322cec1c238a7670b357c3c
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-06-05 22:43:01 +0200

    Initial Commit

---
+++ ColorSwitch/ColorSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSwitchApp.swift
+//  ColorSwitch
+//
+//  Created by Dorian Emenir on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
